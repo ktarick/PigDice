73c3dd7908e996d40c7ba74df0a72ce09373e83d
Kellan Arick <kellan5015@gmail.com>
2019-02-07 15:54:08 -0500

    Initial Commit

--- PD/Program.cs
+++ PD/Program.cs
@@ -10,6 +10,34 @@ namespace PD
     {
         static void Main(string[] args)
         {
+            Console.WriteLine($"Start");
+            Random rnd = new Random();
+            int BestTotal = 0;
+            int BestRolls = 0;
+            uint counter = 0;
+            int gp = 0;
+            ulong GamesToPlay = 1000000000000000000; // 1,000,000,000,000,000,000
+            while (counter++ < GamesToPlay)
+            {
+                int Total = 0;
+                int Rolls = 0;
+                int Die = 0;
+                do
+                {
+                    Total += Die;
+                    Die = rnd.Next(6) + 1;
+                    Rolls++;
+                }
+                while (Die != 1);
+                if (Total > BestTotal)
+                {
+                    BestTotal = Total;
+                    BestRolls = Rolls;
+                    Console.WriteLine($"Score: {BestTotal} in {BestRolls} rolls. Played Games: {counter}");
+                }
+                //continue;
+            }
+            Console.ReadKey();
         }
     }
 }
